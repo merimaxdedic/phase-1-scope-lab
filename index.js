8bffe9e8c9d7f402602ab5615990bb6e23d0a164
Merima Dedic <merimaxdedic@gmail.com>
2023-11-07 20:49:41 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function getCustomerName() {
+  return customerName;
+}
+
+console.log(getCustomerName());
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+    function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+
+
+  "use strict";
+
+const leastFavoriteCustomer = "John";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Jane"; 
+}
+
+changeLeastFavoriteCustomer();
